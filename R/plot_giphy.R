03776c4843c40e855d9db518fa46419d1846197f
SimonGoring <simon.j.goring@gmail.com>
2017-03-14 21:53:49 -0700

    Now we can plot from the `search` result.  The plotting is pretty simple right now.

---
+++ R/plot_giphy.R
@@ -0,0 +1,28 @@
+#' @title Plot a GIF
+#' @description Returns a random GIF from Giphy, which may be limited using tags.
+#' @param image a \code{giphy} object returned from one of the \{giphy} commands.
+#' @param n When the \code{giphy} contains multiple images, the index of the image requested.
+#' @param type GIF size, either \code{original} or \code{downscaled}.  Default is \code{original}.
+#'
+#' @importFrom magick iimage_read
+#' @importFrom httr content GET
+#'
+#' @author Simon J. Goring \email{simon.j.goring@@gmail.com}
+#' @details
+#'
+#' @example
+#'
+
+plot_giphy <- function(image, n = 1, type = 'original', action = TRUE) {
+
+  if (type %in% 'downsized') {
+    image_out <- image$images$downsized$url[n]
+  } else if (type %in% 'original') {
+    image_out <- image$images$original$url[n]
+  }
+
+  out_fig <- magick::image_read(image_out)
+
+  return(out_fig)
+
+}
