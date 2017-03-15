317dac603642cd2e14fffca5317080bacfcac8d3
SimonGoring <simon.j.goring@gmail.com>
2017-03-14 21:32:30 -0700

    Building the `search` function for the `giphy` package.  Should work.  Modelled on the `neotoma` package, in part, and the giphy API [documentation](https://github.com/Giphy/GiphyAPI).

---
+++ R/search.R
@@ -0,0 +1,46 @@
+#' @title Giphy search
+#' @description Search all Giphy GIFs for a word or phrase. Punctuation will be stripped and ignored.  The search can be limited to \
+#' @param q Search query term or phrase
+#' @param limit - The number of results to return, up to 100. The default is \code{limit=25}.
+#' @param offset - An offset, to manage limited returns.  The default is \code{offset=0}, for subsequent searches, can increment \code{offset} by the size of \code{limit}
+#' @param rating - A rating for age appropriate gifs.  Options include \code{"y"}, \code{"g"}, \code{"pg"}, \code{"pg-13"} and \code{"r"}.
+#' @param lang - Regional content; see the allowed 2-letter ISO 639-1 country codes listed in the Details.
+#' @param sticker Should a GIF be returned or an animated sticker (with transparent background)?  Default to \code{FALSE}.
+#' @param api_key - Giphy provides a default \code{api_key} value for public beta testing: \code{"dc6zaTOxFJmzC"}.  This is the default value.
+#'
+#' @importFrom jsonlite fromJSON
+#' @importFrom httr content GET
+#'
+#' @author Simon J. Goring \email{simon.j.goring@@gmail.com}
+#' @details
+#'
+#' @example
+#'
+
+search <- function(q, limit = 25, offset = 0, rating = NULL, lang = NULL, sticker = FALSE, api_key = 'dc6zaTOxFJmzC') {
+
+  params <- as.list(environment())
+
+  params$fmt <- 'json'
+
+  if (params$sticker == TRUE) {
+
+    base_uri <- 'http://api.giphy.com/v1/stickers/search'
+    params$sticker <- NULL
+
+  } else if (params$sticker == TRUE) {
+
+    base_uri <- 'http://api.giphy.com/v1/gifs/search'
+    params$sticker <- NULL
+
+  }
+
+  giphy_response <- httr::content(httr::GET(base_uri, query = params), as = "text")
+  giphy_list <- jsonlite::fromJSON(giphy_response)
+
+  if(giphy_list$meta$status %in% c(400, 401, 403)) {
+    stop(paste0('giphy returned an error: ', meta$msg))
+  }
+
+  return(giphy_list)
+}
