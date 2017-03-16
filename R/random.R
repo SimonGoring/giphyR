899fb8410a2b95292ec38079ab887e492750bfe0
SimonGoring <simon.j.goring@gmail.com>
2017-03-15 22:27:30 -0700

    `random` now generates an object that looks like a larger `giphy` class object, after a lot of mucking around in the code.  It's really ugly, but it works.

--- R/random.R
+++ R/random.R
@@ -38,9 +38,119 @@ random <- function(tag = NULL, rating = NULL, sticker = FALSE, api_key = 'dc6zaT
     stop(paste0('giphy returned an error: ', meta$msg))
   }
 
-  giphy_out <- data.frame(giphy_list$data, stringsAsFactors = FALSE)
+  empty <- data.frame(url = NA,
+                      width = NA,
+                      height = NA,
+                      size = NA, mp4 = NA,
+                      mp4_size = NA, webp = NA,
+                      webp_size = NA)
+
+  empty_still <- data.frame(url = NA,
+                            width = NA,
+                            height = NA,
+                            size = NA)
+
+  empty_mp4 <- data.frame(mp4 = NA, mp4_size = NA, width = NA, height = NA)
+
+  giphy_out <- data.frame(type = giphy_list$data$type,
+                          id   = giphy_list$data$id,
+                          slug = '',
+                          url = giphy_list$data$url,
+                          bitly_gif_url = '',
+                          bitly_url = '',
+                          embed_url = '',
+                          username = giphy_list$data$username,
+                          source = '',
+                          rating = '',
+                          content_url = '',
+                          source_tld = '',
+                          source_post_url = '',
+                          is_indexable = NA,
+                          import_datetime = NA,
+                          trending_datetime = NA,
+                          stringsAsFactors = FALSE)
+
+
+  giphy_out$user <- data.frame(avatar_url = '',
+                               banner_url = '',
+                               profile_url = '',
+                               username = giphy_list$data$username,
+                               display_name = '',
+                               twitter = '')
+
+  giphy_out$images <- data.frame(a = 1)
+
+  giphy_out$images$fixed_height             <- empty
+  giphy_out$images$fixed_height_still       <- empty_still
+  giphy_out$images$fixed_height_downsampled <- data.frame(url = giphy_list$data$fixed_height_downsampled_url,
+                                                          width  = giphy_list$data$fixed_height_downsampled_width,
+                                                          height = giphy_list$data$fixed_height_downsampled_height,
+                                                          webp   = NA, webp_size = NA)
+
+  giphy_out$images$fixed_width             <- empty
+  giphy_out$images$fixed_width_still       <- empty_still
+  giphy_out$images$fixed_width_downsampled <- data.frame(url    = giphy_list$data$fixed_width_downsampled_url,
+                                                         width  = giphy_list$data$fixed_width_downsampled_width,
+                                                         height = giphy_list$data$fixed_width_downsampled_height,
+                                                         webp   = NA, webp_size = NA)
+  giphy_out$images$fixed_height_small      <- data.frame(url    = giphy_list$data$fixed_height_small_url,
+                                                         width  = giphy_list$data$fixed_height_small_width,
+                                                         height = giphy_list$data$fixed_height_small_height,
+                                                         mp4    = NA, mp4_size = NA,
+                                                         webp   = NA, webp_size = NA)
+
+  giphy_out$images$fixed_height_small_still <- data.frame(url   = giphy_list$data$fixed_height_small_still_url,
+                                                          width = NA, height = NA, size = NA)
+  giphy_out$images$fixed_width_small        <- data.frame(url    = giphy_list$data$fixed_height_small_url,
+                                                          width  = giphy_list$data$fixed_height_small_width,
+                                                          height = giphy_list$data$fixed_height_small_height,
+                                                          mp4    = NA, mp4_size = NA,
+                                                          webp   = NA, webp_size = NA)
+
+  giphy_out$images$fixed_width_small_still <- data.frame(url   = giphy_list$data$fixed_width_small_still_url,
+                                                         width = NA, height = NA, size = NA)
+
+  giphy_out$images$downsized       <- empty
+
+  giphy_out$images$downsized_still <- empty_still
+
+  giphy_out$images$downsized_large <- empty_still
+
+  giphy_out$images$downsized_medium <- empty_still
+
+  giphy_out$images$original <- data.frame(url   = giphy_list$data$image_original_url,
+                                          width = giphy_list$data$image_width,
+                                          height = giphy_list$data$image_height,
+                                          size = NA,
+                                          frames = giphy_list$data$image_frames,
+                                          mp4 = giphy_list$data$image_mp4_url,
+                                          mp4_size = NA,
+                                          webp = NA,
+                                          webp_size = NA,
+                                          hash = NA, stringsAsFactors = FALSE)
+
+  giphy_out$images$original_still <- empty_still
+
+  giphy_out$images$looping        <- data.frame(mp4 = NA, mp4_size = NA)
+
+  giphy_out$images$preview <- empty_mp4
+
+  giphy_out$images$downsized_small <- data.frame(mp4 = NA, mp4_size = NA)
+
+  giphy_out$images$preview_gif <- empty_still
+
+  giphy_out$images$original_mp4 <- empty_mp4
+
+  giphy_out$images$hd <- empty_mp4
+
+  giphy_out$images$`480w_still` <- empty_still
+
+  giphy_out$images <- giphy_out$images[,!colnames(giphy_out$images) %in% 'a']
+
   attr(giphy_out, 'meta') <- giphy_list$meta
   attr(giphy_out, 'pagination') <- giphy_list$pagination
+
+  class(giphy_out) <- c('giphy', 'data.frame')
 
   return(giphy_out)
 }
